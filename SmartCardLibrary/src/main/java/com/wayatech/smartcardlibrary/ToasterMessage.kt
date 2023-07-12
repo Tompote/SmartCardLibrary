4300cf5c3f2d5d0c1264101f8c1e4bc601d2eced
Thomas MISRAHI <tmisrahi@waya-tech.com>
2023-07-12 17:06:44 +0200

    Initial Commit

---
+++ SmartCardLibrary/src/main/java/com/wayatech/smartcardlibrary/ToasterMessage.kt
@@ -0,0 +1,11 @@
+package com.wayatech.smartcardlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+
+class ToasterMessage {
+    fun s(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
